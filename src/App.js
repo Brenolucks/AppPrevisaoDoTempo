a31a4311d14ee9dd623f946801953b9585b9a06c
Breno Lucas <brenolucas0106@gmail.com>
2021-04-07 02:15:52 -0300

    Primeira versão do aplicativo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState('');
+
+  useEffect (() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true);
+    })
+  }, [])
+
+  let getWeather = async (lat, long) => {
+    let res = await Axios.get(`https://api.hgbrasil.com/weather?format=json-cors&key=2ba5e459&lat=${lat}&lon=${long}&user_ip=remote` , {
+      params: {
+        lat: lat,
+        long: long,
+      }
+    });
+    setWeather(res.data.results);
+  } 
+
+  if(location === false) {
+    return(
+      <h1> Você deve habilitar a opção de Localização! </h1>
+    );
+  } else {
+  return(
+      <div className="App">
+        <h1> Condições Climaticas: {weather.description}</h1>
+        <ul>
+          <li> Cidade: {weather.city}°C</li>
+          <li> Temperatura: {weather.temp}</li>
+          <li> Data: {weather.date}</li>
+          <li> Hora: {weather.time}</li>
+          <li> Condição climatica: {weather.condition_code}</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
